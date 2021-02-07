0ff3ec873645788b9537668c96356248fe13bd6a
HexBugOrion <58091259+HexBugOrion@users.noreply.github.com>
2021-02-07 01:23:34 -0600

    FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING FUCKING
    I AM SO FUCKING ANGRY

--- src/main/java/net/oriondev/nowhere/registries/configured/NowhereConfiguredFeatures.java
+++ src/main/java/net/oriondev/nowhere/registries/configured/NowhereConfiguredFeatures.java
@@ -9,6 +9,7 @@ import net.oriondev.nowhere.worldgen.WorldGenRegistryHelper;
 import net.oriondev.nowhere.worldgen.biome.NowhereFeatures;
 
 public class NowhereConfiguredFeatures {
+    //God why??
     public static final ConfiguredFeature<NowhereTreeConfig, ?> DRIED_TREE = WorldGenRegistryHelper.createConfiguredFeature("dried_tree", NowhereFeatures.DRIED_TREE.configure(
         new NowhereTreeConfig.Builder().setLeavesBlock(Blocks.AIR).setTrunkBlock(BlockRegistry.DRIED_LOG).setMaxHeight(15).setMinHeight(11).build()));
 
